f4a1021af29712ce3e4d5fce746cb88be8f44b1d
mdshakil04 <m.shakil04@yahoo.com>
2024-03-28 03:08:34 +0600

    Category section added with Swiper Components

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,14 +1,39 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import {Swiper, SwiperSlide} from "swiper/react";
-import {Pagination} from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import {Pagination} from "swiper";
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { FreeMode, Pagination } from 'swiper/modules';
+import slide1 from "../../../assets/home/slide1.jpg";
+import slide2 from "../../../assets/home/slide2.jpg";
+import slide3 from "../../../assets/home/slide3.jpg";
+import slide4 from "../../../assets/home/slide4.jpg";
+import slide5 from "../../../assets/home/slide5.jpg";
 const Category = () => {
     return (
-        <div>
-            
-        </div>
+        <Swiper
+        // className=" container mx-auto"
+        slidesPerView={5}
+        autoplay
+        spaceBetween={35}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper mb-4"
+      >
+        <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide5} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slide3} alt="" /></SwiperSlide>
+      </Swiper>
     );
 };
 
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Category from '../Category/Category';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };

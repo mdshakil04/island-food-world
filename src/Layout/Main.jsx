179cc2c84d188e431324cfadcc5ef8f7f2b3409e
mdshakil04 <m.shakil04@yahoo.com>
2024-03-27 23:32:20 +0600

    Router setup done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Main = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;

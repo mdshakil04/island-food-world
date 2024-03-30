6b0b418f3d672a6fc6b13ee243979e325bcf357b
mdshakil04 <m.shakil04@yahoo.com>
2024-03-31 02:07:45 +0600

    Revies Section done

--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,13 +1,42 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import React, { useEffect, useState } from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+import { FaQuoteLeft } from "react-icons/fa";
 
 const Testimonials = () => {
-    return (
-        <div>
-            <SectionTitle subHeading={'What Our Client Say'} heading={'Testimonials'}></SectionTitle>
-        </div>
-    );
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <div className=" my-8">
+      <SectionTitle
+        subHeading={"What Our Client Say"}
+        heading={"Testimonials"}
+      ></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className=" flex flex-col justify-center items-center gap-4 text-center px-32">
+              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <p className=" text-6xl text-[#CD9003]"><FaQuoteLeft /></p>
+              <p>{review.details}</p>
+              <h3 className="text-2xl text-[#CD9003]">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Testimonials;
